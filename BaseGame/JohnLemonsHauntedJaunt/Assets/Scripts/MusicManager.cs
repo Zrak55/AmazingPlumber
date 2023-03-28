3c402222a00b07ba69ea1558168e64ca83c8438b
Zrak55 <amaxey58@gmail.com>
2023-03-28 10:17:47 -0500

    Class 03/28

--- BaseGame/JohnLemonsHauntedJaunt/Assets/Scripts/MusicManager.cs
+++ BaseGame/JohnLemonsHauntedJaunt/Assets/Scripts/MusicManager.cs
@@ -26,8 +26,30 @@ public class MusicManager : MonoBehaviour
         }
 
         //Code Here: Change source volume to match "Danger" setting
+        if(foundDanger)
+        {
+            dangerMusicSource.volume = Mathf.MoveTowards(dangerMusicSource.volume, 1f, Time.deltaTime);
+            normalMusicSource.volume = Mathf.MoveTowards(normalMusicSource.volume, 0f, Time.deltaTime);
+
+        }
+        else
+        {
+            dangerMusicSource.volume = Mathf.MoveTowards(dangerMusicSource.volume, 0f, Time.deltaTime);
+            normalMusicSource.volume = Mathf.MoveTowards(normalMusicSource.volume, 1f, Time.deltaTime);
+        }
+
 
         //Code Here: Update tracks if a source stops playing
+        if(normalMusicSource.isPlaying == false)
+        {
+            normalMusicSource.clip = normalMusicClips[Random.Range(0, normalMusicClips.Length)];
+            normalMusicSource.Play();
+        }
+        if(dangerMusicSource.isPlaying == false)
+        {
+            dangerMusicSource.clip = dangerMusicClips[Random.Range(0, dangerMusicClips.Length)];
+            dangerMusicSource.Play();
+        }
 
     }
 
